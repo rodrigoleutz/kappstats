688f1dab95bae5262afd9aad33ef2e8fec165c84
Rodrigo Leutz <rodrigo@uware.com.br>
2026-01-12 01:01:42 -0300

    feat: add colors and change visual

--- composeApp/src/commonMain/kotlin/com/kappstats/presentation/core/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/kappstats/presentation/core/MainScreen.kt
@@ -1,13 +1,25 @@
 package com.kappstats.presentation.core
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import com.kappstats.components.part.widget.top_bar.TopBarWidget
+import com.kappstats.components.theme.AppDimensions
+import com.kappstats.components.theme.Blue20
+import com.kappstats.components.theme.Gray60
+import com.kappstats.components.theme.Orange40
 import com.kappstats.presentation.core.state.MainEvent
 import com.kappstats.presentation.core.state.MainUiState
 import com.kappstats.resources.Res
@@ -38,23 +50,36 @@ fun MainScreen(
         Scaffold(
             topBar = {
                 if (uiState.hasTopBar) {
-                    TopBarWidget(
-                        title = uiState.title,
-                        navigationIcon = if (uiState.isBackButton) EvaIcons.Fill.ArrowIosBack
-                        else EvaIcons.Fill.Menu2,
-                        navigationDescription = stringResource(
-                            if (uiState.isBackButton) Res.string.back else Res.string.menu
-                        ),
-                        onNavigationClick = {
-                            if (uiState.isBackButton) {
-                                onEvent(MainEvent.SetIsBackButton(false))
-                            } else scope.launch {
-                                drawerState.open()
-                            }
-                        }
-                    )
+                    Column {
+                        TopBarWidget(
+                            title = uiState.title,
+                            navigationIcon = if (uiState.isBackButton) EvaIcons.Fill.ArrowIosBack
+                            else EvaIcons.Fill.Menu2,
+                            navigationDescription = stringResource(
+                                if (uiState.isBackButton) Res.string.back else Res.string.menu
+                            ),
+                            onNavigationClick = {
+                                if (uiState.isBackButton) {
+                                    onEvent(MainEvent.SetIsBackButton(false))
+                                } else scope.launch {
+                                    drawerState.open()
+                                }
+                            },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = Blue20,
+                                titleContentColor = Color.White,
+                                navigationIconContentColor = Orange40
+                            )
+                        )
+                        Column(
+                            modifier = Modifier.fillMaxWidth()
+                                .height(AppDimensions.Medium.component)
+                                .background(Brush.verticalGradient(listOf(Blue20, Gray60)))
+                        ) { }
+                    }
                 }
-            }
+            },
+            containerColor = Gray60
         ) { innerPadding ->
             LaunchedEffect(innerPadding) {
                 onEvent(MainEvent.SetPaddingValues(innerPadding))
--- composeComponents/src/commonMain/kotlin/com/kappstats/components/theme/AppColors.kt
+++ composeComponents/src/commonMain/kotlin/com/kappstats/components/theme/AppColors.kt
@@ -2,5 +2,21 @@ package com.kappstats.components.theme
 
 import androidx.compose.ui.graphics.Color
 
+
+val Blue20 = Color(0xFF1c2f5e)
+val Blue40 = Color(0xFF1b4d85)
+val Blue80 = Color(0xFF216baa)
+
+val Gray80 = Color(0xFFd3d3d3)
+val Gray60 = Color(0xFFBDBDBD)
+val Gray40 = Color(0xFF848484)
+val Gray20 = Color(0xFF3E3E3E)
 val Green20 = Color(0xFF165A07)
+val Green40 = Color(0xFF388b2c)
+val Green80 = Color(0xFF97dc48)
+
+val Orange20 = Color(0xFFed6813)
+val Orange40 = Color(0xFFee7318)
+val Orange60 = Color(0xFFee9e20)
+val Orange80 = Color(0xFFf3a616)
 val Red20 = Color(0xFF840404)
