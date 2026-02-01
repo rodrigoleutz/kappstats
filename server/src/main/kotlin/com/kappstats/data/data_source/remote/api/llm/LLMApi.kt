10beb78ca6e13bd76583c3725ceb596c2ff4b82b
Rodrigo Leutz <rodrigo@uware.com.br>
2026-01-31 22:47:58 -0300

    feat: composeApp add android icon

---
+++ server/src/main/kotlin/com/kappstats/data/data_source/remote/api/llm/LLMApi.kt
@@ -0,0 +1,5 @@
+package com.kappstats.data.data_source.remote.api.llm
+
+interface LLMApi {
+
+}
---
+++ server/src/main/kotlin/com/kappstats/data/data_source/remote/api/llm/OllamaLLMApiImpl.kt
@@ -0,0 +1,11 @@
+package com.kappstats.data.data_source.remote.api.llm
+
+class OllamaLLMApiImpl(
+    url: String,
+    port: Int,
+    token: String
+): LLMApi {
+
+
+
+}
